7c0600ffd686e377e2f6b07dd44e8fd344df60cf
Carlos Rueda Morales <crueda@gmail.com>
2016-04-07 14:08:41 +0200

    aa

--- send_pending_events.sh
+++ send_pending_events.sh
@@ -20,7 +20,7 @@ import calendar
 import logging, logging.handlers
 import json
 from threading import Thread
-import MySQLdb
+import MySQLdb as mdb
 import requests
 
 ########################################################################
@@ -37,6 +37,7 @@ BBDD_USERNAME = config['BBDD_username']
 BBDD_PASSWORD = config['BBDD_password']
 BBDD_NAME = config['BBDD_name']
 MAX_RETRY = config['max_retry']
+SENT_TIMEOUT = config['sent_timeout']
 
 DEFAULT_SLEEP_TIME = float(config['sleep_time'])
 
@@ -87,19 +88,24 @@ pidfile.close()
 #
 ########################################################################
 
-def sendPendingEvent():
-	 #data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
-	 data = '{"eventType":40,"resourceId":7,"timestamp":"2016-04-05T18:05:42.00Z","source":"KYROS_API"}'
-	 url = 'https://bl.deimos-space.com/WebServicesWMS/rest/listener/notify'
-	 headers = {"Content-type": "application/json", "X-Access": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjY5ODI1MDA1ODksImlzcyI6InN1bW8iLCJyb2xlIjoiYWRtaW5pc3RyYXRvciJ9.CnX6I1puC-H-0AluXge8X4vVlUOfv8x-Nh6VwzxO-n8"}
-	 #resp = requests.post(url, data=data, allow_redirects=True)
-	 response = requests.post(url, headers=headers, data = data, verify=False)
-	 #response = requests.api.request('post', url, data={'bar':'baz'}, verify=False)
-	 print "code:"+ str(response.status_code)
-	 print "******************"
-	 print "headers:"+ str(response.headers)
-	 print "******************"
-	 print "content:"+ str(response.text)
+def sendPendingEvent(url, data):
+	#data = '{"eventType":40,"resourceId":7,"timestamp":"2016-04-05T18:05:42.00Z","source":"KYROS_API"}'
+	#url = 'https://bl.deimos-space.com/WebServicesWMS/rest/listener/notify'
+	headers = {"Content-type": "application/json", "X-Access": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjY5ODI1MDA1ODksImlzcyI6InN1bW8iLCJyb2xlIjoiYWRtaW5pc3RyYXRvciJ9.CnX6I1puC-H-0AluXge8X4vVlUOfv8x-Nh6VwzxO-n8"}
+	
+	try:
+		response = requests.post(url, headers=headers, data = data, verify=False, timeout=0.9)
+		#print "code:"+ str(response.status_code)
+		#print "headers:"+ str(response.headers)
+		#print "content:"+ str(response.text)
+		
+		if (response.status_code!=200):
+			logger.debug("Evento enviado: " + data)
+			return True
+		else:
+			return False
+	except:
+		logger.debug("Error al enviar evento: " + data)
 
 
 ########################################################################
@@ -108,35 +114,49 @@ def sendPendingEvent():
 ########################################################################
 
 def main():
-    con = None
-    try:
-        con = mdb.connect(BBDD_HOST, BBDD_USERNAME, BBDD_PASSWORD, BBDD_NAME)
-        cur = con.cursor()
+	con = None
+	try:
+		con = mdb.connect(BBDD_HOST, BBDD_USERNAME, BBDD_PASSWORD, BBDD_NAME)
+		cur = con.cursor()
 
 		epoch_actual = calendar.timegm(time.gmtime())
-        sql = "SELECT URL, EVENT_JSON_DATA, SENT FROM SUMO_PENDING_EVENT where LIMIT_DATE<" + epoch_actual + " and SENT<" + MAX_RETRY
-        logger.debug("sql: " + sql)
+		sql = "SELECT ID, URL, EVENT_JSON_DATA, SENT FROM SUMO_PENDING_EVENT where LIMIT_DATE<" + str(epoch_actual) + " and SENT<" + str(MAX_RETRY)
+		logger.debug("sql: " + sql)
 		cur.execute(sql)
 		numrows = int(cur.rowcount)
 		if (numrows>0):
 			row = cur.fetchone()
-			url = row[0]
-			data = row[1]
-			retry_counter = row[2]
+			eventId = row[0]
+			url = row[1]
+			data = row[2]
+			retry_counter = row[3]
 			# enviar el evento
+			# Si se ha enviado bien, lo borro sino incremento en contador de reintentos
+			if sendPendingEvent(url, data):
+				#borrar el evento
+				curDelete = con.cursor()
+				sql = "DELETE FROM SUMO_PENDING_EVENT where ID=" + eventId
+				logger.debug("sql: " + sql)
+				curDelete.execute(sql)
+				con.commit()
+				curDelete.close()
+			else:
+				#incrementar el numero de intentos
+				curUpdate = con.cursor()
+				sql = "UPDATE SUMO_PENDING_EVENT set SENT=" + str(retry_counter + 1) + " where ID=" + str(eventId)
+				logger.debug("sql: " + sql)
+				curUpdate.execute(sql)
+				con.commit()
+				curUpdate.close()
 
+	except mdb.Error, e:
+		print "Error %d: %s" % (e.args[0], e.args[1])
+		sys.exit(1)
 
-
-    except mdb.Error, e:
-        print "Error %d: %s" % (e.args[0], e.args[1])
-        sys.exit(1)
-
-    finally:
-        if con:
-            con.close()
+	finally:
+		if con:
+			con.close()
 
 if __name__ == '__main__':
-    #main()
-    sendPendingEvent()
-
-sendPendingEvent()
+    main()
+    #sendPendingEvent()
